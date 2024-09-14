6cb9e434614a0717cdefa365eb6f32da59fd32ca
mike-wurth <mike-wurth@uiowa.edu>
2024-09-13 20:33:12 -0500

    React app general setup, tailwind install, navBar component

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'], // Tailwind CSS v3.x
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
