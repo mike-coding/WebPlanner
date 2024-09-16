bb1088d8a30084a6ea310150c49449fd7239ecdb
mike-wurth <mike-wurth@uiowa.edu>
2024-09-16 13:52:43 -0500

    readme update

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -6,7 +6,7 @@ export default function Todo({storedTasks, clearTasks, addTask, toggleTaskComple
     return (
         <div className="flex flex-col w-full items-center justify-center backdrop-blur-md rounded-3xl border-0 py-20">
             <p className="font-mono font-bold text-stone-700 text-2xl px-80 py-5"> To-Do Prototype </p>
-            <Button className=" w-32 items-center justify-center py-5 shadow-md rounded border rounded-md border-0 bg-stone-100 font-mono font-bold hover:bg-stone-200 px-5 opacity-60" 
+            <Button className=" w-32 items-center justify-center py-5 shadow-md rounded border rounded-md border-0 bg-stone-100 font-mono font-bold githover:bg-stone-200 px-5 opacity-60" 
             onClick={() => {clearTasks(); }}> Wipe Tasks </Button>
             <dl className="items-center justify-center px-80 py-5">
                 <TodoItem addTask={addTask}/>
